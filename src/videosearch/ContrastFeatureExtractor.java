8e1e2517b78963a3f61c9495c491af6d7570433a
zjjcxt <zjjcxt@Adams-MacBook-Pro.local>
2015-05-04 13:54:02 -0700

    fix some bugs

--- src/videosearch/ContrastFeatureExtractor.java
+++ src/videosearch/ContrastFeatureExtractor.java
@@ -9,14 +9,13 @@ import java.awt.image.Raster;
 /**
  * Created by zjjcxt on 4/13/15.
  */
-public class TextFeatureExtractor {
+public class ContrastFeatureExtractor {
     private static final int MAX_IMG_HEIGHT = 64;
     private double[] histogram; // stores all three tamura features in one histogram.
     private int[][] grayScales; //store greyScale of piexls
     private int imgWidth, imgHeight;
 
-    public double[] extract(BufferedImage image) {
-        histogram = new double[2];
+    public double extract(BufferedImage image) {
         ColorConvertOp op = new ColorConvertOp(image.getColorModel().getColorSpace(),
                 ColorSpace.getInstance(ColorSpace.CS_GRAY),
                 new RenderingHints(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY));
@@ -33,70 +32,8 @@ public class TextFeatureExtractor {
         }
         imgWidth = bimg.getWidth();
         imgHeight = bimg.getHeight();
-        histogram[0] = this.coarseness(imgWidth, imgHeight);
-        histogram[1] = this.contrast();
-        return histogram;
-    }
-
-    public double coarseness(int n0, int n1) {
-        double result = 0;
-        for (int i = 1; i < n0 - 1; i++) {
-            for (int j = 1; j < n1 - 1; j++) {
-                result = result + Math.pow(2, this.sizeLeadDiffValue(i, j));
-            }
-        }
-
-        result = (1.0 / (n0 * n1)) * result;
-        return result;
-    }
-
-    public int sizeLeadDiffValue(int x, int y) {
-        double result = 0, tmp;
-        int maxK = 1;
-
-        for (int k = 0; k < 3; k++) {
-            tmp = Math.max(this.differencesBetweenNeighborhoodsHorizontal(x, y, k),
-                    this.differencesBetweenNeighborhoodsVertical(x, y, k));
-            if (result < tmp) {
-                maxK = k;
-                result = tmp;
-            }
-        }
-        return maxK;
-    }
-
-    public double differencesBetweenNeighborhoodsHorizontal(int x, int y, int k) {
-        double result = 0;
-        result = Math.abs(this.averageOverNeighborhoods(x + (int) Math.pow(2, k - 1), y, k) -
-                this.averageOverNeighborhoods(x - (int) Math.pow(2, k - 1), y, k));
-        return result;
-    }
-
-    public double differencesBetweenNeighborhoodsVertical(int x, int y, int k) {
-        double result = 0;
-        result = Math.abs(this.averageOverNeighborhoods(x, y + (int) Math.pow(2, k - 1), k) -
-                this.averageOverNeighborhoods(x, y - (int) Math.pow(2, k - 1), k));
-        return result;
-    }
-
-    public double averageOverNeighborhoods(int x, int y, int k) {
-        double result = 0, border;
-        border = Math.pow(2, 2 * k);
-        int x0 = 0, y0 = 0;
-
-        for (int i = 0; i < border; i++) {
-            for (int j = 0; j < border; j++) {
-                x0 = x - (int) Math.pow(2, k - 1) + i;
-                y0 = y - (int) Math.pow(2, k - 1) + j;
-                if (x0 < 0) x0 = 0;
-                if (y0 < 0) y0 = 0;
-                if (x0 >= imgWidth) x0 = imgWidth - 1;
-                if (y0 >= imgHeight) y0 = imgHeight - 1;
-                result = result + grayScales[x0][y0];
-            }
-        }
-        result = (1 / Math.pow(2, 2 * k)) * result;
-        return result;
+        double contrastIndex = this.contrast();
+        return contrastIndex;
     }
 
     public double contrast() {
